dbc7492758868e81e519b212aec3cb124054a822
Lucas Silva <lcsd.lucas@gmail.com>
2022-04-29 22:31:03 -0300

    Day 2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
 import requests
 import sys
+import json
+
+
+def exibir_valores(lista: list) -> None:
+    for i in lista:
+        print(i)
+
 
 token = sys.argv[1]
 
 response = requests.get(f'https://imdb-api.com/en/API/Top250Movies/{token}')
 
-print(response.text)
+json = json.loads(response.text)["items"]
+
+titulos = []
+imagens_url = []
+anos = []
+notas = []
+
+for filme in json:
+    titulos.append(filme["title"])
+    imagens_url.append(filme["image"])
+    anos.append(filme["year"])
+    notas.append(filme["imDbRating"])
+
+exibir_valores(titulos)
+exibir_valores(imagens_url)
+exibir_valores(anos)
+exibir_valores(notas)
